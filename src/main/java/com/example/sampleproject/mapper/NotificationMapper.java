87d188fda3f9b87d21e42077571b647672dfeec7
shinji <sshin8916@gmail.com>
2020-08-26 21:13:18 +0900

    update

--- src/main/java/com/example/sampleproject/mapper/NotificationMapper.java
+++ src/main/java/com/example/sampleproject/mapper/NotificationMapper.java
@@ -1,20 +1,20 @@
-package com.example.sampleproject.mapper;
+// package com.example.sampleproject.mapper;
 
-import java.util.Optional;
+// import java.util.Optional;
 
-import com.example.sampleproject.entity.Notification;
+// import com.example.sampleproject.entity.Notification;
 
-import org.apache.ibatis.annotations.Mapper;
+// import org.apache.ibatis.annotations.Mapper;
 
-@Mapper
-public interface NotificationMapper {
-	public int searchLatestNotificationInfo(Notification notification);
+// @Mapper
+// public interface NotificationMapper {
+// 	public int searchLatestNotificationInfo(Notification notification);
 
-	public int updateLatestNotificationInfo(Notification notification);
+// 	public int updateLatestNotificationInfo(Notification notification);
 
-	public void insertLatestNotificationInfo(Notification notification);
+// 	public void insertLatestNotificationInfo(Notification notification);
 
-	public Optional<Notification> getLatestCreated(int followee_id, int follower_id);
+// 	public Optional<Notification> getLatestCreated(int followee_id, int follower_id);
 
-	public void deleteNotification(int followee_id, int follower_id);
-}
+// 	public void deleteNotification(int followee_id, int follower_id);
+// }
