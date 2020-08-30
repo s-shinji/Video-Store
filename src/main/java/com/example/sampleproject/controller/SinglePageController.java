9ec4e70e4d8d0df35078e65fdc3142fefc2cc399
shinji <sshin8916@gmail.com>
2020-08-30 17:26:59 +0900

    Update SinglePageController.java

--- src/main/java/com/example/sampleproject/controller/SinglePageController.java
+++ src/main/java/com/example/sampleproject/controller/SinglePageController.java
@@ -2,8 +2,11 @@ package com.example.sampleproject.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+//springとreactでurlがかぶらないようにするため追加
+@RequestMapping("/KdiJ362")
 public class SinglePageController {
 	@GetMapping("/**/{path:[^.]*}")
     public String any() {
