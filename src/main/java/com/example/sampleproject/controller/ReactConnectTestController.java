b806daad22e4c1a9b50d72f2851e7f9277edf207
shinji <sshin8916@gmail.com>
2020-08-30 18:36:45 +0900

    Update ReactConnectTestController.java

--- src/main/java/com/example/sampleproject/controller/ReactConnectTestController.java
+++ src/main/java/com/example/sampleproject/controller/ReactConnectTestController.java
@@ -1,20 +1,22 @@
-// package com.example.sampleproject.controller;
-
+package com.example.sampleproject.controller;
+import org.springframework.stereotype.Controller;
 // import org.springframework.web.bind.annotation.CrossOrigin;
-// import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 // import org.springframework.web.bind.annotation.RequestMapping;
 // import org.springframework.web.bind.annotation.RestController;
 
 // @RestController
 // @CrossOrigin(origins = "http://localhost:3000")
-// // @RequestMapping("/kasa/sample")
-// public class ReactConnectTestController {
-// 	@GetMapping("/getBirthStoneList")
-//     public String[] getBirthStone() {
-// 		String[] birthStone = new String[3];
-// 		birthStone[0] = "ルビー";
-// 		birthStone[1] = "ペリドット";
-// 		birthStone[2] = "サファイア";
-// 	    return birthStone;
-// 	}
-// }
+// @RequestMapping("/kasa/sample")
+@Controller
+public class ReactConnectTestController {
+	@GetMapping("/getBirthStoneList")
+    public String getBirthStone() {
+		// String[] birthStone = new String[3];
+		// birthStone[0] = "ルビー";
+		// birthStone[1] = "ペリドット";
+		// birthStone[2] = "サファイア";
+		// return birthStone;
+		return "login";
+	}
+}
