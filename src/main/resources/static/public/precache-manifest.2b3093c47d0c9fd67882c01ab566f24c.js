7bcf3cce70498524fd2b0d75ef8168cd12111e0e
shinji <sshin8916@gmail.com>
2020-08-30 19:31:16 +0900

    update

---
+++ src/main/resources/static/public/precache-manifest.2b3093c47d0c9fd67882c01ab566f24c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59f10e33441fc59eb27269ab7c397762",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a9e3106f77bdb0f91039",
+    "url": "/static/css/2.bf0413c3.chunk.css"
+  },
+  {
+    "revision": "3f1e24b1846c63b069b9",
+    "url": "/static/css/main.3784639c.chunk.css"
+  },
+  {
+    "revision": "a9e3106f77bdb0f91039",
+    "url": "/static/js/2.c81c3afc.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/static/js/2.c81c3afc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f1e24b1846c63b069b9",
+    "url": "/static/js/main.a9dd6c48.chunk.js"
+  },
+  {
+    "revision": "055e94f6602794288cc3",
+    "url": "/static/js/runtime-main.1f1f19a3.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "ac408c83fed6406fb1db0dedb1adbb05",
+    "url": "/static/media/動画サイト①.ac408c83.jpg"
+  }
+]);
