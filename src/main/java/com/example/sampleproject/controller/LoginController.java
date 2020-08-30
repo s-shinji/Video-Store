4d90b5d32480c1c55b8123687876f338be8f1c49
shinji <sshin8916@gmail.com>
2020-08-30 18:47:34 +0900

    update

--- src/main/java/com/example/sampleproject/controller/LoginController.java
+++ src/main/java/com/example/sampleproject/controller/LoginController.java
@@ -4,7 +4,7 @@ package com.example.sampleproject.controller;
 
 // import org.springframework.security.core.annotation.AuthenticationPrincipal;
 // import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+// import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
