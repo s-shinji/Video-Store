827a9308b0b25f2c9741474459f9b98598693427
shinji <sshin8916@gmail.com>
2020-08-30 16:34:37 +0900

    update

--- frontend/src/actions/index.js
+++ frontend/src/actions/index.js
@@ -16,8 +16,8 @@ export const LOGIN = 'LOGIN'
 export const LOGOUT = 'LOGOUT'
 export const CREATE_REGISTRATION = 'CREATE_REGISTRATION'
 
-const ROOT_URL = 'http://localhost:8080/KdiJ362'
-// const ROOT_URL = '/KdiJ362'
+// const ROOT_URL = 'http://localhost:8080/KdiJ362'
+const ROOT_URL = '/KdiJ362'
 // const ROOT_URL = 'https://fierce-forest-67177.herokuapp.com'
 
 export const readMovieIndex = (loginUserId) => async dispatch => {
