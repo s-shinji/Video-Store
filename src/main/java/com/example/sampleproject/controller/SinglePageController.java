6365e55e9a75c0bfc9c3a181abbe2a5a53636d6b
shinji <sshin8916@gmail.com>
2020-08-31 20:43:02 +0900

    Update SinglePageController.java

--- src/main/java/com/example/sampleproject/controller/SinglePageController.java
+++ src/main/java/com/example/sampleproject/controller/SinglePageController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class SinglePageController {
-	@GetMapping("/**/{path:[^.]*}")
+	@GetMapping("/{path:[^.]*}")
     public String any() {
         return "top";
     }
