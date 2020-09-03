46037841f75c2091fc61895d4140039f8a0e56c7
shinji <sshin8916@gmail.com>
2020-09-03 21:47:16 +0900

    Update DemoApplication.java

--- src/main/java/com/example/sampleproject/DemoApplication.java
+++ src/main/java/com/example/sampleproject/DemoApplication.java
@@ -1,9 +1,11 @@
 package com.example.sampleproject;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.example.sampleproject.mapper")
 public class DemoApplication {
 
 	public static void main(String[] args) {
