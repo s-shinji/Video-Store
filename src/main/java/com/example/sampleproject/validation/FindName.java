3505572bb8cde40cd162abb6bee7e78d14097c59
shinji <sshin8916@gmail.com>
2020-09-03 21:28:04 +0900

    update

--- src/main/java/com/example/sampleproject/validation/FindName.java
+++ src/main/java/com/example/sampleproject/validation/FindName.java
@@ -1,30 +1,30 @@
-// package com.example.sampleproject.validation;
+package com.example.sampleproject.validation;
 
-// import javax.validation.Constraint;
-// import javax.validation.Payload;
-// import java.lang.annotation.Documented;
-// import java.lang.annotation.Retention;
-// import java.lang.annotation.Target;
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
  
-// import static java.lang.annotation.ElementType.FIELD;
-// import static java.lang.annotation.RetentionPolicy.RUNTIME;
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
  
-// @Documented
-// @Constraint(validatedBy = {FindNameValidator.class}) // ここは後に作るバリデーションクラスです。
-// @Target({FIELD}) // 項目に対してバリデーションをかける場合はFIELDを選びます。
-// @Retention(RUNTIME)
-// public @interface FindName {
+@Documented
+@Constraint(validatedBy = {FindNameValidator.class}) // ここは後に作るバリデーションクラスです。
+@Target({FIELD}) // 項目に対してバリデーションをかける場合はFIELDを選びます。
+@Retention(RUNTIME)
+public @interface FindName {
  
-//     String message() default "すでに登録済みのユーザー名です"; // エラーメッセージです。アノテーションの引数にmessageを設定していない場合は、この値が出力されています。
+    String message() default "すでに登録済みのユーザー名です"; // エラーメッセージです。アノテーションの引数にmessageを設定していない場合は、この値が出力されています。
  
-//     Class<?>[] groups() default {};
+    Class<?>[] groups() default {};
  
-//     Class<? extends Payload>[] payload() default {};
+    Class<? extends Payload>[] payload() default {};
  
-//     @Target({FIELD})
-//     @Retention(RUNTIME)
-//     @Documented
-//     public @interface List {
-//         FindName[] value(); // インターフェース名[] value()としておいてください
-//     }
-// }
+    @Target({FIELD})
+    @Retention(RUNTIME)
+    @Documented
+    public @interface List {
+        FindName[] value(); // インターフェース名[] value()としておいてください
+    }
+}
