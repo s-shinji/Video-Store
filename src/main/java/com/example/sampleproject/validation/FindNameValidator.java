54d92b83cd7d1244eafefa96043474a3540a0b0c
shinji <sshin8916@gmail.com>
2020-09-03 21:25:28 +0900

    update

--- src/main/java/com/example/sampleproject/validation/FindNameValidator.java
+++ src/main/java/com/example/sampleproject/validation/FindNameValidator.java
@@ -1,27 +1,27 @@
-package com.example.sampleproject.validation;
+// package com.example.sampleproject.validation;
 
-import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.beans.factory.annotation.Autowired;
 
-import javax.validation.ConstraintValidator;
-import javax.validation.ConstraintValidatorContext;
+// import javax.validation.ConstraintValidator;
+// import javax.validation.ConstraintValidatorContext;
 
-import com.example.sampleproject.entity.MemberRegistrationEntity;
-import com.example.sampleproject.service.RegisterMemberService;
+// import com.example.sampleproject.entity.MemberRegistrationEntity;
+// import com.example.sampleproject.service.RegisterMemberService;
  
-public class FindNameValidator implements ConstraintValidator<FindName, String> {
+// public class FindNameValidator implements ConstraintValidator<FindName, String> {
  
-    @Autowired
-    RegisterMemberService registerMemberService; // ここは各自の設定に合わせてください。
+//     @Autowired
+//     RegisterMemberService registerMemberService; // ここは各自の設定に合わせてください。
  
-    public void initialize(FindName constraintAnnotation) {
-    }
+//     public void initialize(FindName constraintAnnotation) {
+//     }
  
-    public boolean isValid(String value, ConstraintValidatorContext context) {
+//     public boolean isValid(String value, ConstraintValidatorContext context) {
  
-        MemberRegistrationEntity name = registerMemberService.findByName(value); // ここのvalueは入力値になります
-        if(name == null){
-            return true;
-        }
-        return false;
-    }
-}
+//         MemberRegistrationEntity name = registerMemberService.findByName(value); // ここのvalueは入力値になります
+//         if(name == null){
+//             return true;
+//         }
+//         return false;
+//     }
+// }
